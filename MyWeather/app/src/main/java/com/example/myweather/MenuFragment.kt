dfc49891fed78c10d7981a550db0fc70905d212f
igorpavlov17 <iterx73@yandex.ru>
2022-06-01 19:53:36 +0300

    v1.2

--- MyWeather/app/src/main/java/com/example/myweather/MenuFragment.kt
+++ MyWeather/app/src/main/java/com/example/myweather/MenuFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioButton
-import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class MenuFragment : Fragment() {
